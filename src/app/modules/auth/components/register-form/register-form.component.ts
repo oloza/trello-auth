485a999f3f4037e060a8ef45c3aa25225aba573c
oloza <olozra@gmail.com>
2024-11-09 18:41:44 -0400

    class eighth finished

--- src/app/modules/auth/components/register-form/register-form.component.ts
+++ src/app/modules/auth/components/register-form/register-form.component.ts
@@ -46,11 +46,11 @@ export class RegisterFormComponent {
       this.status = 'loading';
       const { name, email, password } = this.form.getRawValue();
       console.log(name, email, password);
-      this.authService.register(name,email,password)
+      this.authService.registerAndLogin(name,email,password)
       .subscribe({
         next:()=>{
           this.status = 'success';
-          this.router.navigate(['/login'])
+          this.router.navigate(['/app/boards'])
         },
         error:(error)=>{
           this.status = 'failed';
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '@environments/environment';
+import { switchMap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -27,9 +28,23 @@ export class AuthService {
     });
   }
 
+  registerAndLogin(name:string,email:string,password:string){
+    return this.register(name,email,password)
+    .pipe(
+      switchMap(()=>this.login(email,password))
+    )
+  }
+
   isAvalable(email:string){
     return this.http.post<{isAvailable:boolean}>(`${this.apiUrl}/api/v1/auth/is-available`,{email});
   }
 
+  recovery(email:string){
+    return this.http.post(`${this.apiUrl}/api/v1/auth/recovery`,{email});
+  }
+
+  changePassword(token: string, newPassword: string) {
+    return this.http.post(`${this.apiUrl}/api/v1/auth/change-password`,{token,newPassword});
+  }
 
 }
